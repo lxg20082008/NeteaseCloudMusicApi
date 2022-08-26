7bda800149e1ed70d2a765df5ed3f9b51a8ef057
wujiyu115 <wjy151486@alibaba-inc.com>
2022-08-26 12:57:21 +0800

    update filenamify

--- util/filenamify.js
+++ util/filenamify.js
@@ -1,8 +1,48 @@
 'use strict'
 const path = require('path')
-const trimRepeated = require('trim-repeated')
-const filenameReservedRegex = require('filename-reserved-regex')
-const stripOuter = require('strip-outer')
+
+const escapeStringRegexp = (string) => {
+  if (typeof string !== 'string') {
+    throw new TypeError('Expected a string')
+  }
+
+  // Escape characters with special meaning either inside or outside character sets.
+  // Use a simple backslash escape when it’s always valid, and a `\xnn` escape when the simpler form would be disallowed by Unicode patterns’ stricter grammar.
+  return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d')
+}
+
+const trimRepeated = (string, target) => {
+  if (typeof string !== 'string' || typeof target !== 'string') {
+    throw new TypeError('Expected a string')
+  }
+
+  const regex = new RegExp(`(?:${escapeStringRegexp(target)}){2,}`, 'g')
+
+  return string.replace(regex, target)
+}
+
+const filenameReservedRegex = () => {
+  return /[<>:"/\\|?*\u0000-\u001F]/g
+}
+const windowsReservedNameRegex = () => {
+  return /^(con|prn|aux|nul|com\d|lpt\d)$/i
+}
+
+const stripOuter = (string, substring) => {
+  if (typeof string !== 'string' || typeof substring !== 'string') {
+    throw new TypeError('Expected a string')
+  }
+
+  if (string.startsWith(substring)) {
+    string = string.slice(substring.length)
+  }
+
+  if (string.endsWith(substring)) {
+    string = string.slice(0, -substring.length)
+  }
+
+  return string
+}
 
 // Doesn't make sense to have longer filenames
 const MAX_FILENAME_LENGTH = 100
@@ -36,7 +76,7 @@ const filenamify = (string, options = {}) => {
     string = string.length > 1 ? stripOuter(string, replacement) : string
   }
 
-  string = filenameReservedRegex.windowsNames().test(string)
+  string = windowsReservedNameRegex().test(string)
     ? string + replacement
     : string
   string = string.slice(0, MAX_FILENAME_LENGTH)
